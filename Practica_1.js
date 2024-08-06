807a6915b4e3b3e36e51271b5ea745b556f4ce3c
alxavila1 <alxavila12@gmail.com>
2024-08-06 18:24:31 -0300

    first commit

---
+++ Practica_1.js
@@ -0,0 +1,37 @@
+var x = 10;
+var y = 20;
+
+function add(x, y){
+    return x + y;
+}
+
+var result = add(10, 20);
+
+var user = new Object();
+user.name = "John Doe";
+user.email = "alxavila12@gmail.com";
+user.age = "27";
+user.login = function() {
+    25991360
+};
+
+user.logout = function(){
+
+};
+
+class user{
+    constructor(){
+        this.name = "John Doe";
+        this.email = "alxavila12@gmail.com";
+        this.age = "27";
+    }
+    login(){//code to log user in
+    }
+
+    logout(){//code to log the user out
+    }    
+}
+
+var user = new user();
+console.log(username); // "John Doe"
+user.login();
